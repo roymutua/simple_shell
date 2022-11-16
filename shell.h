0b218989621ace62437d17fa38a5b221c8fce3fd
roymutua <roy.ngure@gmail.com>
2022-11-16 09:50:40 +0000

    shell.h

--- shell.h
+++ shell.h
@@ -10,3 +10,18 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
+extern char **environ;
+
+int _printenv(void);
+char *_strdup(char *string);
+int cmd_read(char *s, size_t __attribute__((unused))file_stream);
+char *pathfinder(char *cmd);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+void print_not_found(char *cmd);
+int call_command(char *cmd_arr[]);
+char *_getenv(char *var);
+int _str_n_cmp(char *s1, char *s2, int n);
+#endif /* shell_h */
