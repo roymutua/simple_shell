76f0397ff759aef716a9bf2a914712e66f7124db
roymutua <roy.ngure@gmail.com>
2022-11-16 09:48:14 +0000

    man_1_simple_shell

---
+++ shell.h
@@ -0,0 +1,12 @@
+#ifndef shell_h
+#define shell_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/type.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
